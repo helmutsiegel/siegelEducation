70f3f7ab81bedb31f95a84d9f1a99b44d637de34
Helmut Siegel <helmutsiegel@yahoo.com>
2017-03-03 22:46:13 +0200

    the my sqrt

--- siegelEducationJava/src/core/Main.java
+++ siegelEducationJava/src/core/Main.java
@@ -3,7 +3,7 @@ package core;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Heelllo");
+		System.out.println("Heelllo "+MathematicFunctions.mySqrt(5));
 	}
 
 }
---
+++ siegelEducationJava/src/core/MathematicFunctions.java
@@ -0,0 +1,9 @@
+package core;
+
+public class MathematicFunctions {
+	
+	public static double mySqrt(int number){
+		return Math.sqrt(number);
+	}
+	
+}
